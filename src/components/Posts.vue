10ad9876eae67e251030699eec7063affdc0056c
EFugier <emilienfugier@gmail.com>
2018-04-01 19:06:54 +0200

    Added new post interface

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -1,12 +1,23 @@
 <template>
   <div class="posts">
     <h1>Posts</h1>
-    This page lists all the posts.
-    <br>- - -<br>
-
-    <div v-for="post in posts" :key="post.tilte">
-      <h3>{{ post.title }}</h3>
-      <p>{{ post.description }}</p>
+    This page lists all the posts.<br>
+    - - -<br>
+    <!-- <router-link v-bind:to="{ name: 'NewPost' }" class="add_post_link">Add Post</router-link> -->
+    <br>
+    <button class="goto_new_post_btn" @click="gotoNewPost">New Post</button><br>
+    <br>
+    <br>
+    <div v-if="posts.length > 0" class="post-list">
+      <div v-for="post in posts" :key="post.title">
+        <h3>{{ post.title }}</h3>
+        <p>{{ post.description }}</p>
+        <router-link v-bind:to="{ name: 'EditPost', params: { id: post._id } }">Edit</router-link> |
+        <a href="#">Delete</a>
+      </div>
+    </div>
+    <div v-else>
+      There are no posts.. Lets add one now<br>
     </div>
   </div>
 </template>
@@ -27,7 +38,25 @@ export default {
     async getPosts () {
       const response = await PostsService.fetchPosts()
       this.posts = response.data.posts
+    },
+
+    async gotoNewPost () {
+      this.$router.push({ name: 'NewPost' })
     }
   }
 }
 </script>
+
+<style type="text/css">
+.goto_new_post_btn {
+  background: #4d7ef7;
+  color: #fff;
+  padding: 10px 80px;
+  text-transform: uppercase;
+  font-size: 12px;
+  font-weight: bold;
+  width: 250px;
+  border: none;
+  cursor: pointer;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,28 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/HelloWorld'
+import HelloWorld from '@/components/HelloWorld'
 import Posts from '@/components/Posts'
+import NewPost from '@/components/NewPost'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [{
     path: '/',
-    name: 'Hello',
-    component: Hello
+    name: 'HelloWorld',
+    component: HelloWorld
   },
+
   {
     path: '/posts',
     name: 'Posts',
     component: Posts
+  },
+
+  {
+    path: '/posts/new',
+    name: 'NewPost',
+    component: NewPost
   }
   ]
 })
