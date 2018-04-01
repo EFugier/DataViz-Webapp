508f93dee035fe87b8a40ced91f4a1a1bb9b20ba
EFugier <emilienfugier@gmail.com>
2018-04-01 03:20:56 +0200

    Added post display

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -1,14 +1,33 @@
 <template>
   <div class="posts">
-    This file will list all the posts.
+    <h1>Posts</h1>
+    This page lists all the posts.
+    <br>- - -<br>
+
+    <div v-for="post in posts" :key="post.tilte">
+      <h3>{{ post.title }}</h3>
+      <p>{{ post.description }}</p>
+    </div>
   </div>
 </template>
 
 <script>
+import PostsService from '@/services/PostsService'
 export default {
   name: 'posts',
   data () {
-    return {}
+    return {
+      posts: []
+    }
+  },
+  mounted () {
+    this.getPosts()
+  },
+  methods: {
+    async getPosts () {
+      const response = await PostsService.fetchPosts()
+      this.posts = response.data
+    }
   }
 }
 </script>
